0dad56304d2a041ace7803a6c36566b01e0adfb9
Muallimaqiz2003 <robiyahakimova2003@gmail.com>
2024-02-28 21:00:38 +0500

    Domains

---
+++ Domains/Models/AdminLogIn.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domains.Models
+{
+    public class AdminLogIn
+    {
+        public string Adminname { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Domains/Models/CustomerModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domains.Models
+{
+    public class CustomerModel
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Gmail {  get; set; }
+        public string Address { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
