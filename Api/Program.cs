131ed84b566f1a87b64098468883f94c9d91b86c
Muallimaqiz2003 <robiyahakimova2003@gmail.com>
2024-03-02 10:17:46 +0500

    authentificatio

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,6 +1,10 @@
 
 using Application;
 using Infastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 namespace Api
 {
     public class Program
@@ -19,8 +23,70 @@ namespace Api
             builder.Services.AddServices();
             builder.Services.AddInfrastructureServices(builder.Configuration);
 
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Lesson Auth", Version = "v1.0.0", Description = "Lesson Auth API" });
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+                c.AddSecurityDefinition("Bearer", securitySchema);
+                var securityRequirement = new OpenApiSecurityRequirement
+                {
+                    { securitySchema, new[] { "Bearer" } }
+                };
+                c.AddSecurityRequirement(securityRequirement);
+            });
 
 
+
+
+
+
+
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                           .AddJwtBearer(
+                               options =>
+                               {
+                                   options.TokenValidationParameters = GetTokenValidationParameters(builder.Configuration);
+
+                                   options.Events = new JwtBearerEvents
+                                   {
+                                       OnAuthenticationFailed = (context) =>
+                                       {
+                                           if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                                           {
+                                               context.Response.Headers.Add("IsTokenExpired", "true");
+                                           }
+                                           return Task.CompletedTask;
+                                       }
+                                   };
+                               });
+
+            static TokenValidationParameters GetTokenValidationParameters(IConfiguration configuration)
+            {
+                return new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration["JWT:ValidIssuer"],
+                    ValidAudience = configuration["JWT:ValidAudience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"])),
+                    ClockSkew = TimeSpan.Zero,
+                };
+            }
 
 
 
@@ -35,12 +101,13 @@ namespace Api
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
-
 
             app.MapControllers();
 
             app.Run();
         }
+        
     }
 }
